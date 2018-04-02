64c2bde02a4263a2c4f3a36db361c728c281d15c
hongwu <2689164274@qq.com>
2018-04-02 22:42:36 +0800

    加入了数据库和表的各项配置

---
+++ app/src/main/java/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,50 @@
+package coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by DELL on 2018/4/2.F:\CoolWeather\app\src\main\java\coolweather\com\coolweather\db\Province.java
+ * F:\CoolWeather\app\src\main\java\coolweather\com\coolweather\db\Province.java
+ * coolweather.com.coolweather.db.Province
+ *
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
